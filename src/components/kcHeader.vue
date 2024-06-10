563b0e0bc5953af4c004d23f4ee1f2f57d136436
Daan Reuvers <77575704+DaanRVRS@users.noreply.github.com>
2024-06-10 11:05:57 +0200

    added posts

--- src/components/kcHeader.vue
+++ src/components/kcHeader.vue
@@ -1,16 +1,25 @@
 <template> 
+<div class="image">
+  <img src="/KinkyCat_Logo_NoText.png" alt="logo" />
+  <img src="/borabillen.png" alt="logo" />
+</div>
     <header>
       <div class="dropdown">
-        <i class="fa-solid fa-bars" style="font-size:65px;color:white;"></i>
+        <i class="fa-solid fa-bars"></i>
       </div>
       <div class="logo">
         <img src="/KinkyCat_Logo.png" alt="logo" />
       </div>
       <ul>
-        <li><a href="#">Treding</a></li>
+        <li><a href="#">Trending</a></li>
         <li><a href="#">Over Ons</a></li>
         <li><a href="#">Privacy Policy</a></li>
       </ul>
+      <div class="user-container">
+        <div class="user  ">
+          <i class="fa-solid fa-user"></i>
+        </div>
+      </div>
     </header>
 </template>
 
