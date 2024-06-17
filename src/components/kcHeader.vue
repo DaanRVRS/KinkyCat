bad492272b6ee0f5a1a03c2c22eb3a33f869bbe0
BoraEfe <Bora.e@icloud.com>
2024-06-17 10:52:16 +0200

    nav

--- src/components/kcHeader.vue
+++ src/components/kcHeader.vue
@@ -1,11 +1,43 @@
 <template> 
+<nav>
+  <div class="upper-header-container">
+    <div class="searchbar">
+      <input type="text" placeholder="Zoeken" />
+    </div>
+    <div class="close-icon">
+      <i class="fa-solid fa-times fa-3x" v-on:click="toggleMenuClose()"></i>
+    </div>
+  </div>
+  <ul>
+    <li>natte poezen</li>
+    <li>bubblegum pink poezen</li>
+    <li>schattige poezen</li>
+    <li>harige poezen</li>
+    <li>naakte poezen</li>
+    <li>BBC</li>
+    <li>Rode poezen</li>
+    <li>petite poezen</li>
+    <li>Eindhovense poezen</li>
+    <li>Catiestyle</li>
+    <li>rawr</li>
+    <li>Triple poes</li>
+    <li>Gepiercste poes</li>
+    <li>Moslim poes</li>
+    <li>Kitten poes</li>
+    <li>Tompouce</li>
+    <li>Jonge poezen</li>
+    <li>StepCat</li>
+    <li>duo poes</li>
+    <li>Turkse poes</li>
+ </ul>
+</nav>
 <div class="image">
   <img src="/KinkyCat_Logo_NoText.png" alt="logo" />
   <img src="/borabillen.png" alt="logo" />
 </div>
     <header>
       <div class="dropdown">
-        <i class="fa-solid fa-bars"></i>
+        <i class="fa-solid fa-bars" v-on:click="toggleMenu()"></i>
       </div>
       <div class="logo">
         <img src="/KinkyCat_Logo.png" alt="logo" />
@@ -24,15 +56,25 @@
 </template>
 
 <script>
+import { doc } from 'firebase/firestore';
+
 export default {
   data() {
     return {
     };
   },
   methods: {
+    toggleMenu: function () {
+      console.log('toggleMenu');
+      document.querySelector('nav').style.display = 'flex'; 
+    },
+    toggleMenuClose: function () {
+      console.log('toggleMenuClose');
+      document.querySelector('nav').style.display = 'none'; 
+    }
   },
   created() {
 
-  }
+  },
 };
 </script>
