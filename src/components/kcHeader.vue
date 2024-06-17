f75c0180149b04afb05a3ef073d6159fbba2e7fa
Jayr0 <122980334+Jayr0@users.noreply.github.com>
2024-06-17 10:52:17 +0200

    sticky header

--- src/components/kcHeader.vue
+++ src/components/kcHeader.vue
@@ -11,9 +11,9 @@
         <img src="/KinkyCat_Logo.png" alt="logo" />
       </div>
       <ul>
-        <li><a href="#">Trending</a></li>
-        <li><a href="#">Over Ons</a></li>
-        <li><a href="#">Privacy Policy</a></li>
+        <li><a href="kcTrending.vue">Trending</a></li>
+        <li><a href="kcAboutUs.vue">Over Ons</a></li>
+        <li><a href="kcPrivacy.vue">Privacy Policy</a></li>
       </ul>
       <div class="user-container">
         <div class="user  ">
