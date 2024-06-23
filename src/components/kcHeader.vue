d8524441e5049338fe2f7788f90a725546ba32cd
Daan Reuvers <77575704+DaanRVRS@users.noreply.github.com>
2024-06-23 20:02:02 +0200

    added sidebar

--- src/components/kcHeader.vue
+++ src/components/kcHeader.vue
@@ -10,7 +10,7 @@
         <img src="/KinkyCat_Logo.png" alt="logo" class="logo" />
       </RouterLink>
       <div class="dropdown">
-        <i class="fa-solid fa-bars fa-3x"></i>
+        <i class="fa-solid fa-bars fa-3x" @click="toggleSidebar()"></i>
         <i class="fa-solid fa-magnifying-glass fa-2x"></i>
       </div>
       <nav>
@@ -26,16 +26,65 @@
         </RouterLink>
       </div>
     </div>
+    <div class="sidebar">
+      <i class="fa-solid fa-times fa-2x close-sidebar" @click="toggleSidebar()"></i>
+      <nav>
+        <ul>
+          <li>natte poezen</li>
+          <li>bubblegum pink poezen</li>
+          <li>schattige poezen</li>
+          <li>harige poezen</li>
+          <li>naakte poezen</li>
+          <li>BBC</li>
+          <li>Rode poezen</li>
+          <li>petite poezen</li>
+          <li>Eindhovense poezen</li>
+          <li>Catiestyle</li>
+          <li>rawr</li>
+          <li>Triple poes</li>
+          <li>Gepiercste poes</li>
+          <li>Moslim poes</li>
+          <li>Kitten poes</li>
+          <li>Tompouce</li>
+          <li>Jonge poezen</li>
+          <li>StepCat</li>
+          <li>duo poes</li>
+          <li>Turkse poes</li>
+        </ul>
+      </nav>
+    </div>
   </header>
+
 </template>
 
 <script>
 export default {
   data() {
     return {
+      sidebarToggled: false,
     };
   },
   methods: {
+    toggleSidebar: async function () {
+      function delay(time) {
+        return new Promise(resolve => setTimeout(resolve, time));
+      }
+
+      if (this.sidebarToggled === true) {
+        this.sidebarToggled = false;
+        document.querySelector('.sidebar').classList.add('sidebar-slide-out');
+        await delay(500);
+        document.querySelector('.sidebar').classList.remove('sidebar-slide-out');
+        document.querySelector('.sidebar').style.display = 'none';
+      } else {
+        this.sidebarToggled = true;
+        document.querySelector('.sidebar').style.display = 'flex';
+        document.querySelector('.sidebar').classList.add('sidebar-slide-in');
+        await delay(500);
+        document.querySelector('.sidebar').classList.remove('sidebar-slide-in');
+
+      }
+    },
   },
   created() {
 
--- src/pages/kcLoginPage.vue
+++ src/pages/kcLoginPage.vue
@@ -1,15 +1,27 @@
 <template> 
   <div class="loginpage" v-if="state === false">
+    <RouterLink to="/">
+      <strong class="back-to-home">< Back to homepage</strong>
+    </RouterLink>
     <div class="login-card">
+      <RouterLink to="/">
+        <img src="/KinkyCat_Logo.png">
+      </RouterLink>
       <h1>Login</h1>
       <input type="text" placeholder="email" />
       <input type="password" placeholder="password" />
       <button>Login</button>
-      <p class="switch-state">Don't have an account? <a href="#" @click="state = !state">Register</a></p>
+      <p class="switch-state">Don't have an account? <a href="#" @click="state = true">Register</a></p>
     </div>
   </div>
   <div class="registerpage" v-else>
+    <RouterLink to="/">
+      <strong class="back-to-home">< Back to homepage</strong>
+    </RouterLink>
     <div class="register-card">
+      <RouterLink to="/">
+        <img src="/KinkyCat_Logo.png">
+      </RouterLink>
       <h1>Register</h1>
       <input type="text" placeholder="email" />
       <input type="text" placeholder="username" />
